ce53c614d675a90a8ab68616b034dd6413d59bb2
catdad-bots <51088742+catdad-bots@users.noreply.github.com>
2019-05-28 10:11:55 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,60 @@
+// server.js
+// where your node app starts
+
+// Add bot to server: https://discordapp.com/oauth2/authorize?client_id=579382058816634880&scope=bot&permissions=268576768
+
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const commands = require('./commands.js')
+const GuildDb = require('./guild_db.js')
+
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3({region: 'us-east-1', params: {Bucket: process.env.S3_BUCKET}})
+
+const guildDbs = {}
+
+async function initGuildDb(guild, s3) {
+  let db = new GuildDb(guild.id, s3)
+  return await db.init()
+}
+
+async function persistAll(dbList) {
+  console.log('persisting', Object.keys(dbList))
+}
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async msg => {
+  let content_parts = msg.content.split('\n')
+  
+  for (let line of content_parts) {
+    let cmdInfo = commands.parseToCommand(line)
+    if (!cmdInfo) return false
+
+    console.log(`Processing command in ${msg.guild.id}`)
+    console.log("retrieving guild DB")
+    // if the command is recognized, ensure we have the guild DB available
+    if (!guildDbs[msg.guild.id]) guildDbs[msg.guild.id] = await initGuildDb(msg.guild, S3)
+    console.log(`executing ${cmdInfo.command}`)
+    await cmdInfo.handler(cmdInfo, msg, guildDbs[msg.guild.id])
+  }
+})
+
+
+async function main() {
+  client.login(process.env.DISCORD_TOKEN)
+  
+  // setInterval(() => {
+  //   dbUtils.persistAll(guildDbs)
+  // }, 1000)
+}
+
+
+
+
+main()
+
